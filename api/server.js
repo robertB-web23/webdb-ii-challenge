a14733f63bf7b2a6df1b383897fe6a38e5043bc8
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-05 19:08:11 -0500

    mvp met with two stretch goals

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require("helmet");
 
 // bring in routes
 
+const carRouter = require('../cars/cars-router.js');
+
 const server = express();
 
 
@@ -13,7 +15,7 @@ server.use(express.json());
 
 // invoke my routes
 
-
+server.use('/api/cars', carRouter)
 
 
 module.exports = server;
